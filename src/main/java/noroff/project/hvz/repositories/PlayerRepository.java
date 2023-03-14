a309bfa01d6b6cedbb7516efb4f69ed81740b6d0
jetrosa <jetro.saarti@gmail.com>
2023-03-14 16:04:25 +0200

    Add custom find function

--- src/main/java/noroff/project/hvz/repositories/PlayerRepository.java
+++ src/main/java/noroff/project/hvz/repositories/PlayerRepository.java
@@ -4,6 +4,9 @@ import noroff.project.hvz.models.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
+    Set<Player> findAllByGameId(final int gameId);
 }
