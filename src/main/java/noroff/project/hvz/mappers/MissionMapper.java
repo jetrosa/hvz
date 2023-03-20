92c581e75b3f424cb1d1d1079ebf5c32125c74a3
jetrosa <jetro.saarti@gmail.com>
2023-03-20 16:42:50 +0200

    Fix mission POST and PUT (dto)

--- src/main/java/noroff/project/hvz/mappers/MissionMapper.java
+++ src/main/java/noroff/project/hvz/mappers/MissionMapper.java
@@ -10,13 +10,20 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Set;
+
 @Mapper(componentModel = "spring")
 public abstract class MissionMapper {
     @Autowired
     protected GameService gameService;
     public abstract MissionGetDto toMissionDto(Mission mission);
+    public abstract Set<MissionGetDto> toMissionDto(Set<Mission> mission);
     @Mapping(target = "game", source = "gameId", qualifiedByName = "gameIdToGame")
     public abstract Mission toMission(MissionPostDto missionPostDto, int gameId);
+
+    @Mapping(target = "game", source = "gameId", qualifiedByName = "gameIdToGame")
+    @Mapping(target = "id", source = "missionId")
+    public abstract Mission toMission(MissionPostDto missionPostDto, int gameId, int missionId);
 
     @Named("gameIdToGame")
     Game mapGameIdToGame(int id) {
