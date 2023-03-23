6fbae6bbd8f55929a01964adaeb174b641153de7
jetrosa <jetro.saarti@gmail.com>
2023-03-23 15:02:14 +0200

    Fix: add player id to squad get dto

--- src/main/java/noroff/project/hvz/models/dtos/SquadGetDto.java
+++ src/main/java/noroff/project/hvz/models/dtos/SquadGetDto.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 @Data
 public class SquadGetDto {
+    private int id;
     private String name;
     private Boolean isHuman;
     private List<PlayerWithNameAndSquadDto> squadMembers;
