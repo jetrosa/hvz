d4ac42b3db574c6363c6d8036b021a5ec6dfc752
jetrosa <jetro.saarti@gmail.com>
2023-03-23 22:33:10 +0200

    enable scheduling

--- src/main/java/noroff/project/hvz/HvzApplication.java
+++ src/main/java/noroff/project/hvz/HvzApplication.java
@@ -2,8 +2,10 @@ package noroff.project.hvz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HvzApplication {
 
 	public static void main(String[] args) {
