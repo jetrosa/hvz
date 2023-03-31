473a14fc7eada8c24aaa55aa3b4ef8af2ce8f448
samuelahw <samuel.ahjoniemi@gmail.com>
2023-03-31 18:27:21 +0300

    add: security control

--- src/main/java/noroff/project/hvz/config/SecurityConfig.java
+++ src/main/java/noroff/project/hvz/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package noroff.project.hvz.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -19,17 +22,54 @@ public class SecurityConfig {
                 .csrf().disable()
                 // Enable security for http requests
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/game").permitAll()
+
+                        //Security matchers for game-controller
+                        .requestMatchers(HttpMethod.GET,"/api/v1/game").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/v1/game").hasRole("hvz_admin")
+                        .requestMatchers(HttpMethod.DELETE,"/api/v1/game/{gameId}").hasRole("hvz_admin")
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/game/{gameId}").hasRole("hvz_admin")
+
+                        //Security matchers for squad-controller
+                        .requestMatchers(HttpMethod.DELETE,"/api/v1/game/{gameId}/squad/**").hasRole("hvz_admin")
+                        .requestMatchers(HttpMethod.PUT,"/api/v1/game/{gameId}/squad/**").hasRole("hvz_admin")
+
+                        //Security matchers for game-player-controller
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/game/{gameId}/player/**").hasRole("hvz_admin")
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/game/{gameId}/player/**").hasRole("hvz_admin")
+
+                        //Security matchers for mission-controller
+                        .requestMatchers(HttpMethod.POST,"/api/v1/game/{gameId}/mission/**").hasRole("hvz_admin")
+                        .requestMatchers(HttpMethod.DELETE,"/api/v1/game/{gameId}/mission/**").hasRole("hvz_admin")
+                        .requestMatchers(HttpMethod.PUT,"/api/v1/game/{gameId}/mission/**").hasRole("hvz_admin")
+
+                        //Security matchers for bite-controller
+                        .requestMatchers(HttpMethod.DELETE,"/api/v1/game/{gameId}/bite/**").hasRole("hvz_admin")
+
+                        //Security matchers for swagger
                         .requestMatchers("/swagger-ui/").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/**").permitAll()
-                        .requestMatchers("/api/v1/squad/**").hasRole("hvz_user")
+
                         // All other endpoints are protected
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer()
-                .jwt();
+                .jwt()
+                .jwtAuthenticationConverter(jwtRoleAuthenticationConverter());
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtRoleAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        // Use roles claim as authorities
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+        // Add the ROLE_ prefix - for hasRole
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 }
 
